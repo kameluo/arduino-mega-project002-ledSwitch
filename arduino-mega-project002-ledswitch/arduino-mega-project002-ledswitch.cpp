02b593165897de06d1739c69da8418b275c8166c
kamel <ahmedkamel_1988@hotmail.com>
2019-03-20 13:00:21 +0100

    first commit

---
+++ arduino-mega-project002-ledswitch/arduino-mega-project002-ledswitch.cpp
@@ -0,0 +1,30 @@
+#include <arduino.h>
+int switchPin=8;
+int ledPin=13;
+boolean lastButton=false;
+boolean currentButton=false;
+int ledLevel=0;
+void setup() {
+  // initaliaze pins as utputs
+  pinMode(ledPin,OUTPUT);
+  pinMode(switchPin,INPUT);
+
+}
+boolean debouce(boolean last){
+  boolean current=digitalRead(switchPin);
+  if(last!=current){
+    delay(5);
+    current=digitalRead(switchPin);
+    }
+    return current;
+}
+
+void loop() {
+currentButton=debouce(lastButton);
+  if (currentButton==HIGH  && lastButton==false){
+    ledLevel = ledLevel+51;
+  }
+      lastButton==currentButton;
+      if(ledLevel>255) ledLevel=0;
+  analogWrite(ledPin,ledLevel);
+}
